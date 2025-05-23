641a5ad9c07d82b148b36461915a5f492cab5118
itorly <itorly@163.com>
2025-05-23 15:53:32 +0800

    Infinity

--- Numbers/Numbers.js
+++ Numbers/Numbers.js
@@ -47,3 +47,17 @@ let z = x + y;  // "NaN5"
 
 typeof NaN; // "number"
 
+// # 7 Infinity
+// Infinity (or -Infinity) is the value JavaScript will return 
+// if you calculate a number outside the largest possible number.
+let myNumber = 2;
+// Execute until Infinity
+while (myNumber != Infinity) {
+  myNumber = myNumber * myNumber;
+}
+
+// Division by 0 (zero) also generates Infinity:
+let x =  2 / 0; //  Infinity
+let y = -2 / 0; //  -Infinity
+
+typeof Infinity;    // "number"
