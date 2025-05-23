82d8de1a14a7c77b182cb0f9d407e90c88286c4e
itorly <itorly@163.com>
2025-05-23 15:17:23 +0800

    with or without decimals, scientific (exponent) notation

---
+++ Numbers/Numbers.js
@@ -0,0 +1,8 @@
+// # 1 with or without decimals
+var x = 3.14;    // A number with decimals
+var y = 3;       // A number without decimals
+
+// # 2 scientific (exponent) notation
+x = 123e5;    // 12300000
+y = 123e-5;   // 0.00123
+
