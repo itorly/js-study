d41a0ded5d45ced1676c1c61495e33d15483162e
itorly <itorly@163.com>
2025-05-23 15:21:05 +0800

    Integer Precision, Floating Precision

--- Numbers/Numbers.js
+++ Numbers/Numbers.js
@@ -6,3 +6,19 @@ var y = 3;       // A number without decimals
 x = 123e5;    // 12300000
 y = 123e-5;   // 0.00123
 
+// JavaScript Numbers are Always 64-bit Floating Point
+// This format stores numbers in 64 bits, 
+// where the number (the fraction) is stored in bits 0 to 51, 
+// the exponent in bits 52 to 62, and the sign in bit 63
+// Value (aka Fraction/Mantissa)	Exponent	        Sign
+// 52 bits (0 - 51) 	            11 bits (52 - 62)	1 bit (63)
+
+// # 3 Integer Precision
+x = 999999999999999;   // x will be 999999999999999
+y = 9999999999999999;  // y will be 10000000000000000
+
+// # 4 Floating Precision
+// Floating point arithmetic is not always 100% accurate:
+let x = 0.2 + 0.1;
+// To solve the problem above, it helps to multiply and divide:
+let x = (0.2 * 10 + 0.1 * 10) / 10;
