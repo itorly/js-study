bf9348f2876a4fd7fac6256986459983b0f240d6
itorly <itorly@163.com>
2025-05-25 23:59:28 +0800

    Object Default Values

--- Destructuring/Destructuring.js
+++ Destructuring/Destructuring.js
@@ -10,3 +10,16 @@ const person = {
 var {firstName, lastName} = person;
 console.log(firstName); // John
 console.log(lastName); // Doe
+
+
+// 1.1. Object Default Values
+const person_1 = {
+  firstName_1: "John",
+  lastName_1: "Doe",
+  age: 50
+};
+// Destructuring
+let {firstName_1, lastName_1, country = "US"} = person_1;
+console.log(country); // US
+console.log(firstName_1); // John
+console.log(lastName_1); // Doe
