7d21cef00f9df0b06f66154e8ddea8a4586344a9
itorly <itorly@163.com>
2025-05-25 23:55:09 +0800

    Object Destructuring

---
+++ Destructuring/Destructuring.js
@@ -0,0 +1,12 @@
+// 1. Object Destructuring
+// Create an Object
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50
+};
+
+// Destructuring
+var {firstName, lastName} = person;
+console.log(firstName); // John
+console.log(lastName); // Doe
