0b9ab0967aeda3e61c2f80a20ceefe4fca38635a
itorly <itorly@163.com>
2025-05-27 11:35:39 +0800

    Module: Export

---
+++ Modules/Export/Named Exports/person.js
@@ -0,0 +1,14 @@
+/**
+ * In-line individually:
+ */
+export const name = "Jesse";
+export const age = 40;
+
+
+/**
+ * All at once at the bottom:
+ */
+const name2 = "Jesse";
+const age2 = 40;
+
+export {name2, age2};
