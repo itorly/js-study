6238870662d1f92503a2d71950e8a7be99ceff0c
itorly <itorly@163.com>
2025-05-27 11:35:52 +0800

    Module: Import

---
+++ Modules/Import/import_example.js
@@ -0,0 +1,6 @@
+import { name, age } from "../Export/Named Exports/person.js";
+import message from "../Export/Default Exports/message.js";
+
+console.log("name:", name)
+console.log("age:", age)
+console.log("message():", message());
