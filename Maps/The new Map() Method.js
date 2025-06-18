2e49fa45e2f2063072114b35acfd55366f34e37f
itorly <itorly@163.com>
2025-06-18 23:58:46 +0800

    The new Map() Method

---
+++ Maps/The new Map() Method.js
@@ -0,0 +1,9 @@
+// Create a Map
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+let numOfApples = fruits.get("apples");
+console.log("numOfApples", numOfApples);
