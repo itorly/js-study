8553df0d555b784245c3ade218df153f51f5fe95
itorly <itorly@163.com>
2025-06-19 23:53:19 +0800

    Sets: forEach(function(value){}), values()

--- Sets/Sets.js
+++ Sets/Sets.js
@@ -13,3 +13,26 @@ typeof letters;      // Returns object
 letters instanceof Set;  // Returns true
 
 let hasD = letters.has("d");
+
+// The forEach() Method
+// List all entries
+let text = "";
+letters.forEach (function(value) {
+  text += value;
+})
+
+// The values() Method
+// Get all Values
+const myIterator = letters.values();
+
+// List all Values
+let text_1 = "";
+for (const entry of myIterator) {
+  text_1 += entry;
+}
+
+// List all Values
+let text_2 = "";
+for (const entry of letters.values()) {
+  text_2 += entry;
+}
