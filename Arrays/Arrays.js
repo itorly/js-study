c3fa298e4ef0dec60b2a6c2c26b9e7463524f91f
itorly <itorly@163.com>
2025-06-20 16:12:53 +0800

    Arrays are Objects: the toString() method

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,4 +1,20 @@
 // Creating an Array
 const cars_0 = ["Saab", "Volvo", "BMW"];
+
+const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
 // Using the JavaScript Keyword new
-const cars = new Array("Saab", "Volvo", "BMW");
+const cars_1 = new Array("Saab", "Volvo", "BMW");
+
+
+// Converting an Array to a String
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let toString = fruits.toString();
+
+// Arrays are Objects
+const person_0 = ["John", "Doe", 46];
+
+const person = {firstName:"John", lastName:"Doe", age:46};
+let firstName = person.firstName;
