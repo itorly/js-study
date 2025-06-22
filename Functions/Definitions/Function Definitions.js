3f525866cd5db412b94ccd03ac651b339b70ca80
itorly <itorly@163.com>
2025-06-22 23:55:59 +0800

    The Function() Constructor

--- Functions/Definitions/Function Definitions.js
+++ Functions/Definitions/Function Definitions.js
@@ -9,3 +9,12 @@ let x1 = myFunction(4, 3);
 const x2 = function (a, b) {return a * b};
 
 let z = x2(4, 3);
+
+// The Function() Constructor
+const myFunction = new Function("a", "b", "return a * b");
+
+let x3 = myFunction(4, 3);
+
+const myFunction1 = function (a, b) {return a * b};
+
+let x4 = myFunction1(4, 3);
