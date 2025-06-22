a2b2cbc2b26b40f1c23b2c5c387c916279bd1b2a
itorly <itorly@163.com>
2025-06-22 23:58:30 +0800

    Arrow Functions

--- Functions/Definitions/Function Definitions.js
+++ Functions/Definitions/Function Definitions.js
@@ -18,3 +18,14 @@ let x3 = myFunction(4, 3);
 const myFunction1 = function (a, b) {return a * b};
 
 let x4 = myFunction1(4, 3);
+
+// Arrow Functions
+// ES5
+var x5 = function(x, y) {
+  return x * y;
+}
+
+// ES6
+const x6 = (x, y) => x * y;
+
+const x7 = (x, y) => { return x * y };
