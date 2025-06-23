352424e963fc02f2f19ad1fa68060c7ec5ef2cc1
itorly <itorly@163.com>
2025-06-23 15:49:12 +0800

    Function Rest Parameter

--- Functions/Function Parameters.js
+++ Functions/Function Parameters.js
@@ -13,3 +13,13 @@ function myFunction2(x, y = 10) {
   return x + y;
 }
 let z2 = myFunction2(5);
+
+// Function Rest Parameter
+function sum(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
+
+let z3 = sum(4, 9, 16, 25, 29, 100, 66, 77);
+
