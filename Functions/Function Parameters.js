75684cfe48a65d4ddefbcb127a99ddf7edbf6c89
itorly <itorly@163.com>
2025-06-23 15:49:40 +0800

    The Arguments Object

--- Functions/Function Parameters.js
+++ Functions/Function Parameters.js
@@ -23,3 +23,25 @@ function sum(...args) {
 
 let z3 = sum(4, 9, 16, 25, 29, 100, 66, 77);
 
+// The Arguments Object
+let z4 = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+
+let z5 = sumAll(1, 123, 500, 115, 44, 88);
+
+function sumAll() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
