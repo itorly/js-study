9935fdcb16f88d518b4858a6b56375aa42bce2b2
itorly <itorly@163.com>
2025-06-23 17:55:36 +0800

    rename some functions; Define variables and receive the return value when calling a function

--- Functions/Function Invocation.js
+++ Functions/Function Invocation.js
@@ -1,16 +1,16 @@
 // 1.Invoking a Function as a Function
-function myFunction(a, b) {
+function myFunction1(a, b) {
   return a * b;
 }
-myFunction(10, 2);           // Will return 20
+let z1 = myFunction1(10, 2);           // Will return 20
 
 // 2.The Global Object
 
 // In a web browser the global object is the browser window.
 // This example returns the window object as the value of this:
-let x = myFunction();            // x will be the window object
+let z2 = myFunction2();            // x will be the window object
 
-function myFunction() {
+function myFunction2() {
   return this;
 }
 
@@ -23,7 +23,7 @@ const myObject1 = {
     return this.firstName + " " + this.lastName;
   }
 }
-myObject1.fullName();         // Will return "John Doe"
+let z3 = myObject1.fullName();         // Will return "John Doe"
 
 const myObject2 = {
   firstName:"John",
@@ -34,18 +34,18 @@ const myObject2 = {
 }
 
 // This will return [object Object] (the owner object)
-myObject2.fullName();
+let z4 = myObject2.fullName();
 
 
 // 4.Invoking a Function with the Constructor
 // This is a function constructor:
-function myFunction(arg1, arg2) {
+function myFunction3(arg1, arg2) {
   this.firstName = arg1;
   this.lastName  = arg2;
 }
 
 // This creates a new object
-const myObj = new myFunction("John", "Doe");
+const myObj = new myFunction3("John", "Doe");
 
 // This will return "John"
-myObj.firstName;
+let z5 = myObj.firstName;
