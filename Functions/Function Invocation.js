2cfdafb347cc9ac9b3dc9be4459f09b1a71fa62c
itorly <itorly@163.com>
2025-06-23 16:58:29 +0800

    Invoking a Function with the Constructor

--- Functions/Function Invocation.js
+++ Functions/Function Invocation.js
@@ -1,10 +1,10 @@
-// Invoking a Function as a Function
+// 1.Invoking a Function as a Function
 function myFunction(a, b) {
   return a * b;
 }
 myFunction(10, 2);           // Will return 20
 
-// The Global Object
+// 2.The Global Object
 
 // In a web browser the global object is the browser window.
 // This example returns the window object as the value of this:
@@ -14,7 +14,8 @@ function myFunction() {
   return this;
 }
 
-// Invoking a Function as a Method
+
+// 3.Invoking a Function as a Method
 const myObject1 = {
   firstName:"John",
   lastName: "Doe",
@@ -34,3 +35,17 @@ const myObject2 = {
 
 // This will return [object Object] (the owner object)
 myObject2.fullName();
+
+
+// 4.Invoking a Function with the Constructor
+// This is a function constructor:
+function myFunction(arg1, arg2) {
+  this.firstName = arg1;
+  this.lastName  = arg2;
+}
+
+// This creates a new object
+const myObj = new myFunction("John", "Doe");
+
+// This will return "John"
+myObj.firstName;
