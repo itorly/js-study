6fcb2dea0d7d4c6c3897b63bbf6dfb364c217483
itorly <itorly@163.com>
2025-06-24 23:24:28 +0800

    Function call: The JavaScript call() Method

---
+++ Functions/Function call.js
@@ -0,0 +1,17 @@
+// The JavaScript call() Method
+const person = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+const person2 = {
+  firstName:"Mary",
+  lastName: "Doe"
+}
+
+// This will return "John Doe":
+let fullName_person1 = person.fullName.call(person1);
