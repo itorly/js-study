db5a829915fd704bc5d0e3fcddf76a1ef2e6452d
itorly <itorly@163.com>
2025-06-24 23:50:36 +0800

    The JavaScript apply() Method

---
+++ Functions/Function apply().js
@@ -0,0 +1,14 @@
+// The JavaScript apply() Method
+const person = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const person1 = {
+  firstName: "Mary",
+  lastName: "Doe"
+}
+
+// This will return "Mary Doe":
+let fullName_person1 = person.fullName.apply(person1);
