84698832ab4145068cda12aa8b1fc972c6fb91f6
itorly <itorly@163.com>
2025-06-25 00:00:19 +0800

    The apply() Method with Arguments

--- Functions/Function apply().js
+++ Functions/Function apply().js
@@ -12,3 +12,22 @@ const person1 = {
 
 // This will return "Mary Doe":
 let fullName_person1 = person.fullName.apply(person1);
+
+// The Difference Between call() and apply()
+// The difference is:
+// The call() method takes arguments separately.
+// The apply() method takes arguments as an array.
+
+// The apply() Method with Arguments
+const person_args = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person_args1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+let fullName_person_args1 = person_args.fullName.apply(person_args1, ["Oslo", "Norway"]);
