8d18a5bc69385a9caa398634bb00be35543b2cd4
itorly <itorly@163.com>
2025-06-25 23:26:11 +0800

    Simulate a Max Method on Arrays

--- Functions/Function apply().js
+++ Functions/Function apply().js
@@ -1,4 +1,4 @@
-// The JavaScript apply() Method
+// 1.The JavaScript apply() Method
 const person = {
   fullName: function() {
     return this.firstName + " " + this.lastName;
@@ -18,7 +18,7 @@ let fullName_person1 = person.fullName.apply(person1);
 // The call() method takes arguments separately.
 // The apply() method takes arguments as an array.
 
-// The apply() Method with Arguments
+// 2.The apply() Method with Arguments
 const person_args = {
   fullName: function(city, country) {
     return this.firstName + " " + this.lastName + "," + city + "," + country;
@@ -31,3 +31,13 @@ const person_args1 = {
 }
 
 let fullName_person_args1 = person_args.fullName.apply(person_args1, ["Oslo", "Norway"]);
+
+
+// 3.Simulate a Max Method on Arrays
+let maxNum = Math.max.apply(null, [1,2,3]); // Will also return 3
+
+let maxNum2 = Math.max.apply(Math, [1,2,3]); // Will also return 3
+
+let maxNum3 = Math.max.apply(" ", [1,2,3]); // Will also return 3
+
+let maxNum4 = Math.max.apply(0, [1,2,3]); // Will also return 3
