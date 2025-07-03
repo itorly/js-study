a7dc46c4b8d6684168dcb502f8baf7880ff21fb5
itorly <itorly@163.com>
2025-07-03 23:51:11 +0800

    toString() method taking an optional radix

--- Number Methods/Number Methods.js
+++ Number Methods/Number Methods.js
@@ -3,3 +3,6 @@ let x = 123;
 let y1 = x.toString();  // y1 = "123"
 let y2 = (123).toString();  // y2 = "123"
 let y3 = (100 + 23).toString();  // y3 = "123"
+// The toString() method can take an optional radix argument to convert the number to a different base:
+let text = x.toString(2);  // text = "1111011"
+let
