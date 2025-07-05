40e8dcd339444514b46818e19f040f471bec6d2d
itorly <itorly@163.com>
2025-07-05 23:58:50 +0800

    JavaScript MAX_VALUE

--- Number Properties/Number Properties.js
+++ Number Properties/Number Properties.js
@@ -1,3 +1,6 @@
 // JavaScript EPSILON
 // Number.EPSILON is the difference between the smallest floating point number greater than 1 and 1.
 let x = Number.EPSILON;
+
+// JavaScript MAX_VALUE
+x = Number.MAX_VALUE;
